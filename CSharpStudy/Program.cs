dfbb08cab55d0b2ab1c675c2c34092b320f5e24b
Saerom <non1233@naver.com>
2024-01-09 14:55:16 +0900

    docs : 프로그램 기초 프로그램의 기본적인 구조를 알아본다.

--- CSharpStudy/Program.cs
+++ CSharpStudy/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CSharpStudy
+﻿//주석
+//1. //
+//2. /**/
+//3. ///
+
+
+//< using >
+//소스코드의 상단부에 위치, 네임스페이스를 선언
+//네임스페이스 안의 기능을 사용할 수 있다.
+using System; 
+
+namespace CSharpStudy // 기능들을 Programming 이름 아래 묶는다.
 {
+    //클래스
     internal class Program
     {
+
+        //Main 함수 : 프로그램의 시작지점
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            //표준 입출력
+            Console.WriteLine("ㅋㅋㅋ");
+            Console.Write("ㅋㅋ");
+            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
